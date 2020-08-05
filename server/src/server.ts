2335b22505c7c9b07e7432acfa123b2957039936
aloisiomartinez <aloisiojacobs36@gmail.com>
2020-08-05 01:17:51 -0300

    Creating DataBase: Knex sqlite3

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,10 +2,10 @@ import express from 'express';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-    return response.json({
-        nome: 'aloisio'
-    })
+app.use(express.json())
+
+app.post('/users', (request, response) => {
+    console.log(request.body)
 })
 
 app.listen(3333);
