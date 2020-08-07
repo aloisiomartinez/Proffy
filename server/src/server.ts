62da061fc8d8d1799d522ebc898184b227281db9
aloisiomartinez <aloisiojacobs36@gmail.com>
2020-08-07 00:22:15 -0300

    CreateClasses

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,10 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
+app.use(routes);
 
-app.post('/users', (request, response) => {
-    console.log(request.body)
-})
 
 app.listen(3333);
