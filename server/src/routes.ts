f314c16db8648597fdc2403d45ab3ef8bdc312f0
aloisiomartinez <aloisiojacobs36@gmail.com>
2020-08-07 00:52:40 -0300

    CreateClassRoute

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,7 +1,14 @@
 import express from 'express';
 import db from './database/connection';
+import convertHourToMinutes from './utils/convertHourToMinutes';
 
 const routes = express.Router();
+
+interface ScheduleItem {
+    week_day: number;
+    from: string;
+    to: string;
+}
 
 routes.post('/classes', async (request, response) => {
     const {
@@ -14,14 +21,48 @@ routes.post('/classes', async (request, response) => {
         schedule
     } = request.body;
 
-    await db('users').insert({
-        name,
-        avatar,
-        whatsapp,
-        bio,
-    });
+    const trx = await db.transaction()
 
-    return response.send();
+    try {
+        //Retorna os IDs dos users inseridos na tabela
+        const insertedUsersIds = await trx('users').insert({
+            name,
+            avatar,
+            whatsapp,
+            bio,
+        });
+
+        const user_id = insertedUsersIds[0] // Pega somente o primeiro ID (o user que está sendo inserido)
+
+        const insertedClassesIds = await trx('classes').insert({
+            subject,
+            cost,
+            user_id,
+        });
+
+        const class_id = insertedClassesIds[0];
+
+        const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
+            return {
+                class_id,
+                week_day: scheduleItem.week_day,
+                from: convertHourToMinutes(scheduleItem.from),
+                to: convertHourToMinutes(scheduleItem.to),
+            };
+        })
+
+        await trx('class_schedule').insert(classSchedule);
+
+        await trx.commit();
+
+        return response.status(201).send();
+    } catch (err) {
+        await trx.rollback() //Desfaz qualquer alteração que tenha feito no Banco
+
+        return response.status(400).json({
+            error: 'Unexpected error while creating new class'
+        })
+    }
 });
 
 export default routes;
